316100a8e1620520ba063e8209e0979930727f9e
r <pkomarov@redhat.com>
2023-09-17 02:40:30 +0300

    updated main

--- main.go
+++ main.go
@@ -106,7 +106,6 @@ func main() {
 		Client:  mgr.GetClient(),
 		Scheme:  mgr.GetScheme(),
 		Kclient: kclient,
-		Log:     ctrl.Log.WithName("controllers").WithName("PlacementAPI"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "PlacementAPI")
 		os.Exit(1)
