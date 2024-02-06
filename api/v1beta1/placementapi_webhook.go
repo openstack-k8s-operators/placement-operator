b06cab5abf2f44d37c4a4e9b7bde5e4b2a73e004
Brendan Shephard <bshephar@redhat.com>
2024-02-06 11:28:24 +1000

    [webhook] Update webhook functions for CR update
    This change updates the webhook functions to comply with the new
    standard for controller-runtime.
    
    Signed-off-by: Brendan Shephard <bshephar@redhat.com>

--- api/v1beta1/placementapi_webhook.go
+++ api/v1beta1/placementapi_webhook.go
@@ -26,6 +26,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // PlacementAPIDefaults -
@@ -75,25 +76,25 @@ func (spec *PlacementAPISpec) Default() {
 var _ webhook.Validator = &PlacementAPI{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *PlacementAPI) ValidateCreate() error {
+func (r *PlacementAPI) ValidateCreate() (admission.Warnings, error) {
 	placementapilog.Info("validate create", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return nil, nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *PlacementAPI) ValidateUpdate(old runtime.Object) error {
+func (r *PlacementAPI) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	placementapilog.Info("validate update", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return nil, nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *PlacementAPI) ValidateDelete() error {
+func (r *PlacementAPI) ValidateDelete() (admission.Warnings, error) {
 	placementapilog.Info("validate delete", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object deletion.
-	return nil
+	return nil, nil
 }
